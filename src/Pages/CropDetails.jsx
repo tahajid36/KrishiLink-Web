541627b5265cbcb0c26d383877730ee3e1f201d9
tahajid36 <brucextahajid@gmail.com>
2025-11-13 19:54:53 +0600

    interest accept and reject

--- src/Pages/CropDetails.jsx
+++ src/Pages/CropDetails.jsx
@@ -55,18 +55,19 @@ const CropDetails = () => {
         return
       }
 
+
       const InterestInfo ={
         cropId: crop._id,
         userEmail: user.email,
         userName: user.displayName,
-        quantity: quantityReference.current.value ,
+        quantity: Number(quantityReference.current.value) ,
         total: totalReference.current.value,
         message: e.target.message.value,
         status: 'Pending'
         
       }
 
-      console.log(InterestInfo)
+      console.log(InterestInfo.quantity)
 
       Swal.fire({
         title: "Are you sure?",
@@ -101,6 +102,33 @@ const CropDetails = () => {
 
 
     }
+
+    const handleUpdate =async (interestId, newStatus) => {
+      const res = await fetch(`http://localhost:1000/crops/${_id}/interest/${interestId}`,{
+        method: 'PUT',
+        headers: {
+          'content-type': 'application/json'
+        },
+        body: JSON.stringify({status: newStatus})
+
+      })
+      const data = await res.json();
+      if(data.modifiedCount > 0){
+        Swal.fire({
+          position: 'top-end',
+          icon: 'success',
+          title: `Interest ${newStatus}`,
+          showConfirmButton: false,
+          timer: 1500
+        });
+      }
+
+      const updatedInterests = crop.interests.map(i=> 
+        i._id === interestId ? {...i, status: newStatus}: i
+      )
+      crop.interests = updatedInterests
+    }
+    
   return (
     <div>
  <div  className="grid grid-cols-2 mt-20 gap-10 md:gap-20">
@@ -133,7 +161,7 @@ const CropDetails = () => {
                 <input ref={quantityReference} onChange={handleQuantity} name='quantity' type="text" className="input w-full" placeholder="Enter quantity" />
                 
                 <label className="label">Total Price:</label>
-                <input ref={totalReference}  name='total' type="text" className="input w-full" placeholder="Enter quantity" />
+                <input ref={totalReference}  name='total' type="text" className="input w-full" placeholder="total" />
 
                 <label className="label">Message</label>
                 <input  name='message' type="text" className="input w-full" placeholder="Enter your message" />
@@ -143,6 +171,46 @@ const CropDetails = () => {
                 </form>
       
     </div>
+    {(user.email === owner.ownerEmail ?    <div className="mt-20">
+      <h1 className="text-4xl font-semibold my-5 text-center">All Interest Requests for this Crop</h1>
+      <div className="overflow-x-auto rounded-box border border-base-content/5 bg-base-100">
+    <table className="table">
+      {/* head */}
+      <thead>
+        <tr>
+          <th>Sl No.</th>
+          <th>Buyer Name</th>
+          <th>Quantity</th>
+          <th>Message</th>
+          <th>Status</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+      <tbody> 
+        {/* row 1 */}
+        {crop.interests.map(interest => <tr key={interest._id}>
+          <th>1</th>
+          <td>{interest.userName}</td>
+          <td>{interest.quantity}</td>
+          <td>{interest.message}</td>
+          <td>{interest.status}</td>
+          <td> <button 
+          onClick={()=> handleUpdate(interest._id, "accepted")} className="btn-success btn-outline btn-xs btn mr-2">Accept</button>   
+           <button
+           onClick={()=> handleUpdate(interest._id, "rejected")} className="btn-error btn-outline btn-xs btn">Reject</button></td>
+        </tr>)}
+        
+      </tbody>
+    </table>
+  </div>
+      </div> : '') 
+
+     
+    }
+
+    
+
+    
 
     </div>
   );
